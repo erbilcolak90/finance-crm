8b7c8c5a733bbda07dc5e8bbd6db3688b3a3ab9c
Erbil Colak <erbilcolak90@gmail.com>
2023-05-19 21:18:46 +0300

    UpdateUserValidationDocumentStatusService and Test added, updateUserValidationDocumentStatus method added

---
+++ src/test/java/com/financecrm/webportal/admin/UpdateUserValidationDocumentStatusServiceTest.java
@@ -0,0 +1,73 @@
+package com.financecrm.webportal.admin;
+
+import com.financecrm.webportal.entities.UserValidationDocument;
+import com.financecrm.webportal.enums.UserValidationDocumentStatus;
+import com.financecrm.webportal.enums.UserValidationDocumentType;
+import com.financecrm.webportal.input.uservalidationdocument.UpdateUserValidationDocumentStatusInput;
+import com.financecrm.webportal.repositories.UserValidationDocumentRepository;
+import com.financecrm.webportal.services.admin.UpdateUserValidationDocumentStatusService;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Date;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+class UpdateUserValidationDocumentStatusServiceTest {
+
+    @InjectMocks
+    private UpdateUserValidationDocumentStatusService updateUserValidationDocumentStatusService;
+    @Mock
+    private UserValidationDocumentRepository userValidationDocumentRepository;
+
+
+    @BeforeEach
+    void setUp(){
+
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @DisplayName("should Return UserValidationDocument when UserValidationDocumentId Is Exist And IsDeleted False In UserValidationDocuments From UpdateUserValidationDocumentStatusInput")
+    @Tag("updateUserValidationDocumentStatus")
+    @Test
+    void shouldReturnUserValidationDocument_whenUserValidationDocumentIdIsExistAndIsDeletedFalseInUserValidationDocumentsFromUpdateUserValidationDocumentStatusInput(){
+        UpdateUserValidationDocumentStatusInput updateUserValidationDocumentStatusInput = new UpdateUserValidationDocumentStatusInput("test_documentId", UserValidationDocumentStatus.APPROVED);
+        UserValidationDocument savedUserValidationDocument = new UserValidationDocument("test_documentId","test_userId","test_url", UserValidationDocumentType.TEST,UserValidationDocumentStatus.APPROVED,false,new Date(),new Date());
+
+        Mockito.when(userValidationDocumentRepository.findById(updateUserValidationDocumentStatusInput.getDocumentId())).thenReturn(Optional.of(new UserValidationDocument()));
+        Mockito.when(userValidationDocumentRepository.save(ArgumentMatchers.any(UserValidationDocument.class))).thenReturn(savedUserValidationDocument);
+
+        UserValidationDocument expectedResult = updateUserValidationDocumentStatusService.updateUserValidationDocumentStatus(updateUserValidationDocumentStatusInput);
+
+        assertNotNull(expectedResult);
+        assertEquals(UserValidationDocumentStatus.APPROVED, expectedResult.getStatus());
+
+        Mockito.verify(userValidationDocumentRepository).findById(updateUserValidationDocumentStatusInput.getDocumentId());
+        Mockito.verify(userValidationDocumentRepository).save(ArgumentMatchers.any(UserValidationDocument.class));
+    }
+
+    @DisplayName("should Return Null when UserValidationDocumentId Does Not Exist In UserValidationDocuments From UpdateUserValidationDocumentStatusInput")
+    @Tag("updateUserValidationDocumentStatus")
+    @Test
+    void shouldReturnNull_whenUserValidationDocumentIdDoesNotExistInUserValidationDocumentsFromUpdateUserValidationDocumentStatusInput(){
+        UpdateUserValidationDocumentStatusInput updateUserValidationDocumentStatusInput = new UpdateUserValidationDocumentStatusInput("test_documentId", UserValidationDocumentStatus.APPROVED);
+
+        Mockito.when(userValidationDocumentRepository.findById(updateUserValidationDocumentStatusInput.getDocumentId())).thenReturn(Optional.empty());
+
+        UserValidationDocument expectedResult = updateUserValidationDocumentStatusService.updateUserValidationDocumentStatus(updateUserValidationDocumentStatusInput);
+
+        assertNull(expectedResult);
+
+        Mockito.verify(userValidationDocumentRepository).findById(updateUserValidationDocumentStatusInput.getDocumentId());
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        MockitoAnnotations.openMocks(this).close();
+    }
+
+}
