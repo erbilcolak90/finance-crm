dedec5d6a2ded7806d9519b907b201338ebf81b6
Erbil Colak <erbilcolak90@gmail.com>
2023-05-20 22:55:36 +0300

    UpdateUserValidationDocumentStatusService and UpdateUserStatusService class name changed

--- src/main/java/com/financecrm/webportal/controller/admin/AdminUserValidationDocumentStatusController.java
+++ src/main/java/com/financecrm/webportal/controller/admin/AdminUserValidationDocumentStatusController.java
@@ -2,7 +2,7 @@ package com.financecrm.webportal.controller.admin;
 
 import com.financecrm.webportal.entities.UserValidationDocument;
 import com.financecrm.webportal.input.uservalidationdocument.UpdateUserValidationDocumentStatusInput;
-import com.financecrm.webportal.services.admin.UpdateUserValidationDocumentStatusService;
+import com.financecrm.webportal.services.admin.AdminUserValidationDocumentStatusService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,12 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminUserValidationDocumentStatusController {
 
     @Autowired
-    private UpdateUserValidationDocumentStatusService updateUserValidationDocumentStatusService;
+    private AdminUserValidationDocumentStatusService adminUserValidationDocumentStatusService;
 
     @PostMapping("/updateUserValidationDocumentStatus")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<UserValidationDocument> updateUserValidationDocumentStatus(@RequestBody UpdateUserValidationDocumentStatusInput updateUserValidationDocumentStatusInput){
-        UserValidationDocument result = updateUserValidationDocumentStatusService.updateUserValidationDocumentStatus(updateUserValidationDocumentStatusInput);
+        UserValidationDocument result = adminUserValidationDocumentStatusService.updateUserValidationDocumentStatus(updateUserValidationDocumentStatusInput);
         if(result != null){
             return ResponseEntity.ok(result);
         }else{
